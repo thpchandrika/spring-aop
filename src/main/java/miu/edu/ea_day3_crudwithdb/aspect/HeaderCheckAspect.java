f57d00ed139ea3c317d4533353e36845059c9418
thpchandrika <thpchandrika@gmail.com>
2023-05-12 14:24:16 -0500

    initial commit

---
+++ src/main/java/miu/edu/ea_day3_crudwithdb/aspect/HeaderCheckAspect.java
@@ -0,0 +1,36 @@
+package miu.edu.ea_day3_crudwithdb.aspect;
+
+import lombok.RequiredArgsConstructor;
+import miu.edu.ea_day3_crudwithdb.exception.AopIsAwesomeHeaderException;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class HeaderCheckAspect {
+
+    private final HttpServletRequest httpServletRequest;
+
+    // execution(accessModifier fullPackageName.classes.methods(arguments)
+    @Pointcut("execution(* miu.edu.ea_day3_crudwithdb.service.*.*(..))")
+    public void CheckHttpPostHeaderPointCut() {
+
+    }
+
+    @Before("CheckHttpPostHeaderPointCut()")
+    public void CheckHeaderBehaviour(JoinPoint joinpoint)
+            throws AopIsAwesomeHeaderException {
+        if (httpServletRequest.getMethod().equals("POST")) {
+            if (httpServletRequest.getHeader("AOP-IS-AWESOME") == null) {
+                throw new AopIsAwesomeHeaderException("'AOP-IS-AWESOME' is not given in header");
+            }
+        }
+    }
+
+}
